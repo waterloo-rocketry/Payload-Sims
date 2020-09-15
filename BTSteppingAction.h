5d55931b415f9da36c064cbfc6ea95a76b3fd7f2
Delaney Dyment <70232125+dldyment@users.noreply.github.com>
2020-09-15 00:24:02 -0400

    Added RunAction and SteppingAction
    RunAction: defines one "run" in the simulation. defines dose, calculates it, and displays it add the end of the run
    
    SteppingAction: defines the process in one "step" of the program, i.e. firing a particle, figuring out if it reaches the volume for which dose is being measured, and if yes adding the energy deposit to the total

---
+++ BTSteppingAction.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "G4UserSteppingAction.hh"
+#include "globals.hh"
+
+class BTEventAction;
+
+class G4LogicalVolume;
+
+class BTSteppingAction : public G4UserSteppingAction
+{
+public:
+	BTSteppingAction(BTEventAction* eventaction);
+	virtual ~BTSteppingAction();
+
+	virtual void UserSteppingAction(const G4Step*);
+
+private:
+	BTEventAction* fEventAction;
+	G4LogicalVolume* fScoringVolume;
+};
+
+#endif
