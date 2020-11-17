ab66b0fdd00f4856572fbbf954f73c4bff519f6c
pcmodeactivate <349348193@gapps.yrdsb.ca>
2020-11-17 18:18:10 -0500

    changed waterbox to scintillator

--- BTDetectorConstruction.cpp
+++ BTDetectorConstruction.cpp
@@ -1,6 +1,25 @@
 /// \file BTDetectorConstruction.cpp
 /// \brief Implementation of the BTDetectorConstruction class
 
+
+//Next Step: Add an aluminium cover panel on top of the scintillator with the thickness of 2.38 mm and 
+
+// Presumably something like this to define the material if needed.
+
+// #include "G4Material.hh"
+// #include "G4MaterialTable.hh"
+// #include "G4Element.hh"
+// #include "G4ElementTable.hh"
+// ......
+// G4Material* Al = 
+// new G4Material("Aluminium", z=13., a= 26.98*g/mole, density= 2.7*g/cm3);
+
+//Alternatively "G4_Al" can be used as a pre-defined material.
+
+// Geometry-wise G4Box/G4PolyHedra should be enough to build the shape with the addition of 
+// G4UnionSolid, G4SubtractionSolid, G4IntersectionSolid.
+// http://geant4.in2p3.fr/IMG/pdf_Lecture-Geometry.pdf
+
 #include "BTDetectorConstruction.hh"
 
 #include "G4RunManager.hh"
@@ -48,29 +67,29 @@ G4VPhysicalVolume* BTDetectorConstruction::Construct()
             0,                     //copy number
             checkOverlaps);        //check for any overlaps
 
-	// Water Box Parameters
-	G4double wb_sizeXY = 10 * cm, wb_sizeZ = 20 * cm;
-	G4Material* wb_mat = nist->FindBuildMaterial("G4_WATER");
+	// Scintillator Parameters
+	G4double sc_sizeXY = 2 * cm, sc_sizeZ = 1 * cm; //20x20x10 mm
+	G4Material* sc_mat = nist->FindBuildMaterial("G4_PLASTIC_SC_VINYLTOLUENE"); //Scintillator Material
 
-    G4Box* solidwb =
-        new G4Box("Water Box",   //name
-            wb_sizeXY, wb_sizeXY, wb_sizeZ); //size
+    G4Box* scint =
+        new G4Box("Scintillator",   //name
+            sc_sizeXY, sc_sizeXY, sc_sizeZ); //size
 
-    G4LogicalVolume* logicwb =
-        new G4LogicalVolume(solidwb,       //box is solid
-            wb_mat,                        //material
-            "Water Box");                  //name
+    G4LogicalVolume* logicsc =
+        new G4LogicalVolume(scint,         //scint is solid
+            sc_mat,                        //material
+            "Scintillator");               //name
 
     new G4PVPlacement(0,         //no rotation
         G4ThreeVector(),         //at origin
-        logicwb,                 //logical volume
-        "Water Box",             //name
+        logicsc,                 //logical volume
+        "Scintillator",          //name
         logicWorld,              //mother volume
         false,                   //no boolean operation
         0,                       //copy number
         checkOverlaps);          //check for any overlaps
 
-    fScoringVolume = logicwb;
+    fScoringVolume = logicsc;
 
     return physWorld;
 }
