b8cce12c2c97189e90e0d454a0379ce455966c78
willerf <erfwill@gmail.com>
2021-08-18 22:03:40 -0700

    Basic General Particle Source setup

---
+++ build/CMakeFiles/BuildTest.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "BuildTest"
+  "BuildTest.pdb"
+  "CMakeFiles/BuildTest.dir/BuildTest.cpp.o"
+  "CMakeFiles/BuildTest.dir/src/BTActionInitialization.cpp.o"
+  "CMakeFiles/BuildTest.dir/src/BTDetectorConstruction.cpp.o"
+  "CMakeFiles/BuildTest.dir/src/BTEventAction.cpp.o"
+  "CMakeFiles/BuildTest.dir/src/BTPrimaryGeneratorAction.cpp.o"
+  "CMakeFiles/BuildTest.dir/src/BTRunAction.cpp.o"
+  "CMakeFiles/BuildTest.dir/src/BTSteppingAction.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BuildTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
